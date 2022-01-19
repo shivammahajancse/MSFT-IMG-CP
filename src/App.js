ea0ddf5b3197f4bcc2f2c8db6f86e013ba0b435b
Vasu Vijayvargiya <56105511+Vasuvj24@users.noreply.github.com>
2022-01-20 01:15:43 +0530

    completed basic ui

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ function App() {
   const postsData=[{name:'Raju Rastogi',bio:'NIMS School',content:'Hello EveryOne i have just won a competetion of NSO and i would like share with you thank you all for supporting me',like:'10',time:'22:56,7th Jan 2022'},{name:'Raju Rastogi',bio:'NIMS School',content:'Hello EveryOne i have just won a competetion of NSO and i would like share with you thank you all for supporting me',like:'10',time:'22:56,7th Jan 2022'},{name:'Raju Rastogi',bio:'NIMS School',content:'Hello EveryOne i have just won a competetion of NSO and i would like share with you thank you all for supporting me',like:'10',time:'22:56,7th Jan 2022'},{name:'Raju Rastogi',bio:'NIMS School',content:'Hello EveryOne i have just won a competetion of NSO and i would like share with you thank you all for supporting me',like:'10',time:'22:56,7th Jan 2022'},{name:'Raju Rastogi',bio:'NIMS School',content:'Hello EveryOne i have just won a competetion of NSO and i would like share with you thank you all for supporting me',like:'10',time:'22:56,7th Jan 2022'},{name:'Raju Rastogi',bio:'NIMS School',content:'Hello EveryOne i have just won a competetion of NSO and i would like share with you thank you all for supporting me',like:'10',time:'22:56,7th Jan 2022'}]
   const notification=["he called multiple times and redarded","concall on the way","mayham viwed your profile","start preparing for new event"]
   const events = ["ISRO Challenge","NSO Nationals","Best physics Contenst,Agra"]
-  const Profile = {name:""}
+  const nameAbout = [{name:"Vasu Vijayvargiya",bio:"NIMS School",place:'Jaipur Rajasthan'},{about:'I am studying student in NIMS School and national level science olypiard winner 2012'}];
+  const experience = [{experience:'NSO Winner',company:'National level Olympiard',time:'2012'},{experience:'NMO Winner',company:'National level Olympiard',time:'2014'}];
+  const education = [{education:'Shri Guru Tegh Bahadur Academy',class:'8th std'},{education:'Joseph Academy',class:'5th std-8th std'}]
   console.log("LoginIs value at start of app " +LoginIs);
   const admin = {id:"VasuVj24",pass:"123"};
   const receiveData = (obj)=>{
@@ -34,7 +36,7 @@ function App() {
       <Switch>
         <Route path='/' exact component={()=><Home loginIs={LoginIs} events={events} handleLogout={handleLogout} postsData={postsData}/>} />
         <Route path='/Notification' component={()=><Notification notification={notification} loginIs={LoginIs} handleLogout={handleLogout}/>} />
-        <Route path='/me' component={()=><Me postsData={postsData} loginIs={LoginIs} handleLogout={handleLogout}/>} />
+        <Route path='/me' component={()=><Me nameAbout={nameAbout} experience={experience} education={education} loginIs={LoginIs} handleLogout={handleLogout}/>} />
         <Route path='/login' component={()=><Login LoginIs={LoginIs} handleLogout={handleLogout} receiveData={receiveData}/>}/>
         <Route path='/signup' component={Signup}/>
         <Route path='/contact' component={()=><Contactus loginIs={LoginIs} handleLogout={handleLogout}/>}/>
--- src/Components/Me.js
+++ src/Components/Me.js
@@ -4,7 +4,17 @@ import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import './Me.css'
 import TextField from '@mui/material/TextField';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+import EditIcon from '@mui/icons-material/Edit';
 function Me(props) {
+    const [about, setabout] = useState(props.nameAbout[1].about);
+    const [exClicked, setexClicked] = useState(false);
+    const [abClicked, setabClicked] = useState(false)
+    const [exi, setexi] = useState(props.experience)
+    let experience,company,time; 
+    let newAbout;
     if (props.loginIs === false) {
         console.log("inside");
         return <Redirect to="/login" />;
@@ -14,16 +24,43 @@ function Me(props) {
             <Menu LoginIs={props.loginIs} handleLogout={props.handleLogout}></Menu>
             <div className="meParent">
                 <div className="profileImage">
-                    <AccountCircleIcon style={{ fontSize: '17vmin' }}></AccountCircleIcon>
+                {/* <Avatar src="/broken-image.jpg" /> */}
+                    <AccountCircleIcon style={{ fontSize: '15vmin', color: 'grey' }}></AccountCircleIcon>
                     <div className="info">
-                        <h2></h2>
-                        <h3>Admin</h3>
-                        <h4>NIMS School</h4>
-                    </div>
-                    <div className="aboutInfo">
+                        <h2>{props.nameAbout[0].name}</h2>
+                        <h3>{props.nameAbout[0].bio}</h3>
+                        <h4>{props.nameAbout[0].place}</h4>
                     </div>
                 </div>
-                <TextField style={{alignSelf:'flex-start'}} id="standard-basic" label="About" variant="standard" />
+                <div className="about">
+                <div className="headingExpi">
+                    <h2>About</h2><Fab onClick={() => { !abClicked ? setabClicked(true) : setabClicked(false) }} color="secondary" aria-label="edit"><EditIcon /></Fab>
+                    </div>
+                    {abClicked ? <div className="aboutWrite"><TextField onChange={(e) => newAbout = e.target.value} style={{ alignSelf: 'flex-start' }} id="standard-basic" label="About" variant="standard" /><Fab onClick={() => { setabout(newAbout); }} style={{ size: '1%', cursor: 'pointer' }} color="secondary" aria-label="add"><AddIcon /></Fab></div> : console.log("not clicked on about")}
+                    <div className="aboutContent">
+                        {about}
+                    </div>
+                </div>
+                <div className="experience">
+                    <div className="headingExpi">
+                        <h2>Experience</h2><Fab onClick={() => { !exClicked ? setexClicked(true) : setexClicked(false)}} color="secondary" aria-label="edit"><EditIcon /></Fab> 
+                    </div>
+                    {
+                        exClicked ? <div className="exi">
+                        <div className="aboutWrite"><TextField onChange={(e)=>{experience=`${e.target.value}`;console.log(experience)}} style={{ alignSelf: 'flex-start' }} id="standard-basic" label="Experience" variant="standard" /><Fab onClick={() => {setexi([...props.experience+{experience:`${experience}`,company:`${company}`,time:`${time}`}]);console.log("expi "+exi)}} style={{ size: '1%', cursor: 'pointer' }} color="primary" aria-label="add"><AddIcon /></Fab></div>
+                        <div className="aboutWrite"><TextField onChange={(e)=>{company=e.target.value}} style={{ alignSelf: 'flex-start' }} id="standard-basic" label="Company" variant="standard" /></div>
+                        <div className="aboutWrite"><TextField onChange={(e)=>{time=e.target.value}} style={{ alignSelf: 'flex-start' }} id="standard-basic" label="Time" variant="standard" /></div>
+                        </div> : console.log("not showing expi")
+                    }
+                    {exi.map(each => {
+                        return <div className="ExperienceContent">
+                            <h3>{each.experience}</h3>
+                            <h4>{each.company}</h4>
+                            <h5>{each.time}</h5>
+                        </div>
+                    }
+                    )}
+                </div>
             </div>
         </div>
     )
