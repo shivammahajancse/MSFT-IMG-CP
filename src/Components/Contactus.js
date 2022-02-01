65c26ca9b49a3c109e4ad5e79fc535d6b3c09789
Vasu Vijayvargiya <56105511+Vasuvj24@users.noreply.github.com>
2022-02-01 23:03:12 +0530

    final

--- src/Components/Contactus.js
+++ src/Components/Contactus.js
@@ -14,21 +14,30 @@ function Contactus(props) {
             {!props.search ? <div className="contactusChild">
                 <div className="contactusChild1">
                     <h1>What Actually are we?</h1>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laudantium, saepe ducimus in aut illum fugit necessitatibus. Quo velit mollitia inventore error, iure consectetur consequuntur similique, possimus quis ipsa laudantium?
+                    We are a Platform, that aims at making biggest School Student Community around the world to help, explore and educate.
                 </div>
                 <div className="contactusChild1">
-                    <h1>What do We Aim?</h1>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam eligendi nam molestiae corporis nisi? Enim beatae eveniet maiores a inventore doloribus, voluptate earum sit deserunt molestiae sed eos debitis reprehenderit!
+                    <h1>Why did we made it?</h1>
+                    In today's world school students are exceptionally talented but they don't have the right platform to showcase their talent.
+                    Despite having lots of opportunities, these students face difficulties in finding the right opportunity because they are in scattered form.
+                    In the search of the right opportunity, they often get indulged into wrong things and waste a lot of time.
+                    Also, Organisations whose target audience is school students face difficulties in finding them at one place
                 </div>
                 <div className="contactusChild1">
-                    <h1>Why You Should Invest Your Precious Time in us?</h1>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque nobis, doloribus impedit ad veniam, nesciunt laborum dolor rem ratione quod dolore esse nemo odio consequuntur quae corrupti iusto consectetur ab?
+                    <h1>Our Research</h1>
+                    Students don't have a big community to support and explore their talent.
+
+                    Even if they know about the competitions and somehow manage to secure good positions then also they don't have the right platform to share and showcase their skillsWe made a platform ("Acad"), that aims at making biggest School Student Community around the world.
+                    Through this platform, students will get aware about the opportunities and it will also enlarge their field of view.
+                    It will help students to showcase their skillset and increase their reach.
+                    It will also help organizations targeting school students to get maximum reach in minimum investment.
                 </div>
                 <div className="contactusChild2">
                     <h2>Contact Us</h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil soluta minima veniam eos praesentium? Facere enim sit debitis animi quod reprehenderit nulla dolore recusandae commodi, minima, mollitia exercitationem eius tenetur.
+                    <span>00000000000</span>                    
+                    xyz@gmail.com
                 </div>
-            </div> : <Search/>}
+            </div> : <Search />}
         </div>
     )
 }
